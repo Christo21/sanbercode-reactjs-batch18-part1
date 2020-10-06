09c8f70cd3bb60b709dc7e4fe86b8a2aeccfdb7b
dr4co <christoper21jo@gmail.com>
2020-10-06 21:00:05 +0700

    Tugas-7

---
+++ Tugas-7/tugas7.js
@@ -0,0 +1,89 @@
+console.log("================================");
+console.log("Jawaban soal 1");
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+        this.legs = 4;
+        this.cold_blooded = false;
+    }
+    get animalName(){
+        return this.name;
+    }
+    set animalName(x){
+        this.name = x;
+    }
+    get animalLegs(){
+        return this.legs;
+    }
+    get animalCold_blooded(){
+        return this.cold_blooded;
+    }
+}
+
+var sheep = new Animal("shaun");
+
+console.log(sheep.name);
+console.log(sheep.legs);
+console.log(sheep.cold_blooded);
+
+class Frog extends Animal {
+    constructor(name) {
+        super(name);
+    }
+    jump() {
+        console.log("hop hop");
+    }
+}
+
+class Ape extends Animal {
+    constructor(name) {
+        super(name);
+        this.legs = 2;
+    }
+    yell() {
+        console.log("Auooo");
+    }
+}
+
+var sungokong = new Ape("kera sakti");
+sungokong.yell();
+
+var kodok = new Frog("buduk");
+kodok.jump();
+
+console.log("================================");
+console.log("Jawaban soal 2");
+
+class Clock {
+    constructor(template) {
+        this.template = template;
+        this.timer
+    }
+    stop() {
+        clearInterval(this.timer);
+    }
+    start(){
+        this.render();
+        this.timer = setInterval(this.render.bind(this), 1000);
+    }
+    render() {
+        var date = new Date();
+
+        var hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        var mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        var secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        var output = this.template.replace('h', hours).replace('m', mins).replace('s', secs);
+
+        console.log(output);
+    }
+}
+
+var clock = new Clock('h:m:s');
+clock.start(); 
